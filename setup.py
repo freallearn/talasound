42e248b3561235332c17251c2dbd27a93600fc2a
Arnaud <arnaud.frealle@eisti.fr>
2020-03-29 20:11:49 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='talasound',
+    version='',
+    packages=find_packages,
+    url='',
+    license='',
+    author='Arnaud Fréalle, Charles Paillot',
+    author_email='',
+    description=''
+)
